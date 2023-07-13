dd22f34540797927d5d590ef290c9d41787d53e8
RemyaJoby <remyajames@gmail.com>
2023-07-13 04:18:16 -0600

    Adding EmployeeDto

---
+++ WebApi/Models/EmployeeDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models
+{
+    // 3.Create new repository to get and save employee information with the following data model properties:
+    public class EmployeeDto : BaseDto
+    {
+        public string EmployeeCode { get; set; }
+        public string EmployeeName { get; set; }
+        public string CompanyName { get; set; }
+        public string OccupationName { get; set; }
+        public string EmployeeStatus { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime LastModifiedDateTime { get; set; }
+    }
+}
