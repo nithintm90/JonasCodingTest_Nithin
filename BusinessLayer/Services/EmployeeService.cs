eda6e3a6148c3d9d0d360e3989a11bc14483bff2
Raj Patel <raj.patel@flynncompanies.com>
2023-07-16 19:11:08 -0400

    Implement Employee Controller and Repo Methods

---
+++ BusinessLayer/Services/EmployeeService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using BusinessLayer.Model.Models;
+using DataAccessLayer.Model.Interfaces;
+using DataAccessLayer.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Model.Interfaces;
+
+namespace BusinessLayer.Services
+{
+    public class EmployeeService: IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<EmployeeInfo>> GetAllEmployees()
+        {
+            var res = await _employeeRepository.GetAll().ConfigureAwait(false);
+            return _mapper.Map<IEnumerable<EmployeeInfo>>(res);
+        }
+
+        public async Task<EmployeeInfo> GetEmployeeByCode(string employeeCode)
+        {
+            var result = await _employeeRepository.GetByCode(employeeCode).ConfigureAwait(false);
+            return _mapper.Map<EmployeeInfo>(result);
+        }
+
+        public async Task<bool> SaveEmployee(EmployeeInfo employee)
+        {
+            var result = _mapper.Map<Employee>(employee);
+            return await _employeeRepository.SaveEmployee(result).ConfigureAwait(false);
+        }
+
+        public async Task<bool> DeleteEmployee(string employeeCode)
+        {
+            return await _employeeRepository.DeleteEmployee(employeeCode).ConfigureAwait(false);
+        }
+    }
+}
---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using BusinessLayer.Model.Interfaces;
+using BusinessLayer.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IMapper _mapper;
+
+        public EmployeeController(IEmployeeService employeeService, IMapper mapper)
+        {
+            _employeeService = employeeService;
+            _mapper = mapper;
+        }
+        // GET api/<controller>
+        public async Task<IEnumerable<EmployeeDto>> GetAll()
+        {
+            var items = await _employeeService.GetAllEmployees().ConfigureAwait(false);
+            return _mapper.Map<IEnumerable<EmployeeDto>>(items);
+        }
+
+        // GET api/<controller>/5
+        public async Task<EmployeeDto> Get(string employeeCode)
+        {
+            var item = await _employeeService.GetEmployeeByCode(employeeCode).ConfigureAwait(false);
+            return _mapper.Map<EmployeeDto>(item);
+        }
+
+        // POST api/<controller>
+        public async Task<bool> Post([FromBody] EmployeeDto employee)
+        {
+            var employeeInfo = _mapper.Map<EmployeeInfo>(employee);
+            return await _employeeService.SaveEmployee(employeeInfo).ConfigureAwait(false);
+        }
+
+        // PUT api/<controller>/5
+        public async Task<bool> Put(string employeeCode, [FromBody] EmployeeDto employee)
+        {
+            if (employeeCode != employee.EmployeeCode)
+            {
+                throw new Exception("Employee code does not match");
+            }
+            var employeeInfo = _mapper.Map<EmployeeInfo>(employee);
+            return await _employeeService.SaveEmployee(employeeInfo).ConfigureAwait(false);
+        }
+
+        // DELETE api/<controller>/5
+        public async Task<bool> Delete(string employeeCode)
+        {
+            return await _employeeService.DeleteEmployee(employeeCode).ConfigureAwait(false);
+        }
+    }
+}
