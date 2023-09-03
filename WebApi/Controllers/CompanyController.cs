d5071db288b2294dc2638d86ff305c34feb28921
Saeed Aghdam <saeed.snfa@hotmail.com>
2023-09-03 18:32:34 -0400

    Addd logger to solution and add proper error handling

--- WebApi/Controllers/CompanyController.cs
+++ WebApi/Controllers/CompanyController.cs
@@ -1,8 +1,9 @@
 ﻿using AutoMapper;
 using BusinessLayer.Model.Interfaces;
 using BusinessLayer.Model.Models;
+using log4net;
+using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using WebApi.Models;
@@ -13,44 +14,111 @@ namespace WebApi.Controllers
     {
         private readonly ICompanyService _companyService;
         private readonly IMapper _mapper;
+        private readonly ILog _logger;
 
         public CompanyController(ICompanyService companyService, IMapper mapper)
         {
             _companyService = companyService;
             _mapper = mapper;
+            _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         }
         // GET api/company
-        public async Task<IEnumerable<CompanyDto>> GetAllAsync()
+        public async Task<IHttpActionResult> GetAllAsync()
         {
-            var items = await _companyService.GetAllCompaniesAsync();
-            return _mapper.Map<IEnumerable<CompanyDto>>(items);
+            try
+            {
+                _logger.Info("GetAllAsync request");
+                var items = await _companyService.GetAllCompaniesAsync();
+                return Ok(_mapper.Map<IEnumerable<CompanyDto>>(items));
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"GetAllAsync exception , error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // GET api/company/{companyCode}
-        public async Task<CompanyDto> GetAsync(string companyCode)
+        public async Task<IHttpActionResult> GetAsync(string companyCode)
         {
-            var item = await _companyService.GetCompanyByCodeAsync(companyCode);
-            return _mapper.Map<CompanyDto>(item);
+            try
+            {
+                _logger.Info($"GetAsync request, company code: {companyCode}");
+                if (string.IsNullOrEmpty(companyCode))
+                    return BadRequest("The company code can not be empty.");
+
+                var item = await _companyService.GetCompanyByCodeAsync(companyCode);
+                return Ok(_mapper.Map<CompanyDto>(item));
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"GetAsync exception, company code: {companyCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // POST api/company
-        public async Task<bool> PostAsync([FromBody] CompanyDto companyDto)
+        public async Task<IHttpActionResult> PostAsync([FromBody] CompanyDto companyDto)
         {
-            var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
-            return await _companyService.SaveCompanyAsync(companyInfo);
+            try
+            {
+                _logger.Info($"PostAsync request, company code: {companyDto.CompanyCode}");
+                if (companyDto == null)
+                    return BadRequest("The company can not be empty.");
+
+                var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
+                var result = await _companyService.SaveCompanyAsync(companyInfo);
+                _logger.Info($"PostAsync result, company code: {companyDto.CompanyCode}, result:{result}");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"PostAsync exception, company code: {companyDto.CompanyCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // PUT api/company/{companyCode}
-        public async Task<bool> PutAsync(string companyCode, [FromBody] CompanyDto companyDto)
+        public async Task<IHttpActionResult> PutAsync(string companyCode, [FromBody] CompanyDto companyDto)
         {
-            var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
-            return await _companyService.UpdateCompanyAsync(companyInfo, companyCode);
+            try
+            {
+                _logger.Info($"PutAsync request, company code: {companyCode}");
+                if (string.IsNullOrEmpty(companyCode))
+                    return BadRequest("The company code can not be empty.");
+                if (companyDto == null)
+                    return BadRequest("The company can not be empty.");
+
+                var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
+                var result = await _companyService.UpdateCompanyAsync(companyInfo, companyCode);
+                _logger.Info($"PutAsync result, company code: {companyDto.CompanyCode}, result:{result}");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"PutAsync exception, company code: {companyCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // DELETE api/company/{companyCode}
-        public async Task<bool> DeleteAsync(string companyCode)
+        public async Task<IHttpActionResult> DeleteAsync(string companyCode)
         {
-            return await _companyService.DeleteCompanyAsync(companyCode);
+            try
+            {
+                _logger.Info($"DeleteAsync request, company code: {companyCode}");
+                if (string.IsNullOrEmpty(companyCode))
+                    return BadRequest("The company code can not be empty.");
+
+                var result = await _companyService.DeleteCompanyAsync(companyCode);
+                _logger.Info($"DeleteAsync result, company code: {companyCode}, result:{result}");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"DeleteAsync exception, company code: {companyCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
     }
 }
--- WebApi/Controllers/EmployeeController.cs
+++ WebApi/Controllers/EmployeeController.cs
@@ -1,8 +1,9 @@
 using AutoMapper;
 using BusinessLayer.Model.Interfaces;
 using BusinessLayer.Model.Models;
+using log4net;
+using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using WebApi.Models;
@@ -13,45 +14,112 @@ namespace WebApi.Controllers
     {
         private readonly IEmployeeService _employeeService;
         private readonly IMapper _mapper;
+        private readonly ILog _logger;
 
         public EmployeeController(IEmployeeService employeeService, IMapper mapper)
         {
             _employeeService = employeeService;
             _mapper = mapper;
+            _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         }
 
         // GET api/employee
-        public async Task<IEnumerable<EmployeeDto>> GetAllAsync()
+        public async Task<IHttpActionResult> GetAllAsync()
         {
-            var items = await _employeeService.GetAllEmployeesAsync();
-            return _mapper.Map<IEnumerable<EmployeeDto>>(items);
+            try
+            {
+                _logger.Info("GetAllAsync request");
+                var items = await _employeeService.GetAllEmployeesAsync();
+                return Ok(_mapper.Map<IEnumerable<EmployeeDto>>(items));
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"GetAllAsync exception , error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // GET api/employee/{employeeCode}
-        public async Task<EmployeeDto> GetAsync(string employeeCode)
+        public async Task<IHttpActionResult> GetAsync(string employeeCode)
         {
-            var item = await _employeeService.GetEmployeeByCodeAsync(employeeCode);
-            return _mapper.Map<EmployeeInfo, EmployeeDto>(item);
+            try
+            {
+                _logger.Info($"GetAsync request, employee code: {employeeCode}");
+                if (string.IsNullOrEmpty(employeeCode))
+                    return BadRequest("The employee code can not be empty.");
+
+                var item = await _employeeService.GetEmployeeByCodeAsync(employeeCode);
+                return Ok(_mapper.Map<EmployeeInfo, EmployeeDto>(item));
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"GetAsync exception, employee code: {employeeCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // POST api/employee
-        public async Task<bool> PostAsync([FromBody] EmployeeDto employeeDto)
+        public async Task<IHttpActionResult> PostAsync([FromBody] EmployeeDto employeeDto)
         {
-            var employeeInfo = _mapper.Map<EmployeeInfo>(employeeDto);
-            return await _employeeService.SaveEmployeeAsync(employeeInfo);
+            try
+            {
+                _logger.Info($"PostAsync request, employee code: {employeeDto.CompanyCode}");
+                if (employeeDto == null)
+                    return BadRequest("The employee can not be empty.");
+
+                var employeeInfo = _mapper.Map<EmployeeInfo>(employeeDto);
+                var result = await _employeeService.SaveEmployeeAsync(employeeInfo);
+                _logger.Info($"PostAsync result, employee code: {employeeDto.CompanyCode}, result:{result}");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"PostAsync exception, employee code: {employeeDto.CompanyCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // PUT api/employee/{employeeCode}
-        public async Task<bool> PutAsync(string employeeCode, [FromBody] EmployeeDto employeeDto)
+        public async Task<IHttpActionResult> PutAsync(string employeeCode, [FromBody] EmployeeDto employeeDto)
         {
-            var employeeInfo = _mapper.Map<EmployeeInfo>(employeeDto);
-            return await _employeeService.UpdateEmployeeAsync(employeeInfo, employeeCode);
+            try
+            {
+                _logger.Info($"PutAsync request, employee code: {employeeCode}");
+                if (string.IsNullOrEmpty(employeeCode))
+                    return BadRequest("The employee code can not be empty.");
+                if (employeeDto == null)
+                    return BadRequest("The employee can not be empty.");
+
+                var employeeInfo = _mapper.Map<EmployeeInfo>(employeeDto);
+                var result = await _employeeService.UpdateEmployeeAsync(employeeInfo, employeeCode);
+                _logger.Info($"PutAsync result, employee code: {employeeDto.CompanyCode}, result:{result}");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"PutAsync exception, employee code: {employeeCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
 
         // DELETE api/employee/{employeeCode}
-        public async Task<bool> DeleteAsync(string employeeCode)
+        public async Task<IHttpActionResult> DeleteAsync(string employeeCode)
         {
-            return await _employeeService.DeleteEmployeeAsync(employeeCode);
+            try
+            {
+                _logger.Info($"DeleteAsync request, employee code: {employeeCode}");
+                if (string.IsNullOrEmpty(employeeCode))
+                    return BadRequest("The employee code can not be empty.");
+
+                var result = await _employeeService.DeleteEmployeeAsync(employeeCode);
+                _logger.Info($"DeleteAsync result, employee code: {employeeCode}, result:{result}");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"DeleteAsync exception, employee code: {employeeCode}, error: {ex.Message}", ex);
+                return InternalServerError(ex);
+            }
         }
     }
 }
